05e23c7edc36edb8fb6f9857bf8c44272655a798
dxfareed <einstein1101@proton.me>
2025-06-15 04:45:28 +0100

    explorer update

--- contracts/utils.ts
+++ contracts/utils.ts
@@ -1,8 +1,5 @@
-const EmployerPoolContractAddress =
-  "0x4cF0D4dFc2E71C2291B39c7f084E5933091850cC";
+const EmployerPoolContractAddress = "0x4cF0D4dFc2E71C2291B39c7f084E5933091850cC";
 const MOCK_USDC = "0xd587A5C0575E5B072459E83D3AD749073EE1ebb2";
-const base_sepolia_scan = "https://sepolia-explorer.base.org/";
+const base_sepolia_scan = "https://sepolia.basescan.org/";
 
 export { EmployerPoolContractAddress, MOCK_USDC, base_sepolia_scan };
-
-//rpc https://sepolia.base.org/
